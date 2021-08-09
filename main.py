ae133f266ab98a643a3662a3d4bcd86b83d9dfc2
stem league <64894084+stemleague@users.noreply.github.com>
2021-08-09 16:25:37 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# This line allows to use external functions from the Math Library
+import math
+
+if __name__ == "__main__":
+  # TODO: print name of program
+
+  c = int(input("How far is the object from the ground?"))
+  b = int(input("How far is WALL-E’s eye level to object’s position?"))
+  # TODO: compute the distance from WALL-E and item
+
+  # TODO: print out computed distance in a personalized message
